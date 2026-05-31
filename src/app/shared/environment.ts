0c9acb17f4e5ceee467721d8fefb06b8d83bf1d9
Marooo <marwanbayoumiii@gmail.com>
2026-05-31 23:28:47 +0300

    feat: add _redirects file and update API base URL in environment configuration

--- src/app/shared/environment.ts
+++ src/app/shared/environment.ts
@@ -1,7 +1,7 @@
 export const environments = {
     // baseUrl : 'https://unspecialized-joanna-unpopulated.ngrok-free.dev/api'
     // baseUrl: 'https://related-adventures-circuits-giant.trycloudflare.com/api'
-    baseUrl: 'https://lightseagreen-otter-384127.hostingersite.com/api',
-    //baseUrl: 'https://api.bahgatayma.me/api',
+    //baseUrl: 'https://lightseagreen-otter-384127.hostingersite.com/api',
+    baseUrl: 'https://api.bahgatayma.me/api',
 
 }
